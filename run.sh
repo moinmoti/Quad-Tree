8bf388866b087a5862de5c28e73d3a7469125687
Moin Hussain Moti <mhmoti@cse.ust.hk>
2022-05-26 17:17:49 +0800

    Change default page capacity in the run script.

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ meson compile -C build
 #             QF="${q}/${t}-L1e7-I1e6-${w}H"
 #             echo "Experiments/${QF}"
 #             mkdir -p "Experiments/${QF}"
-#             ./KDBTree $PWD $QF 128 128
-#             # lldb -- Index $PWD $QF 128 128
+#             ./Index $PWD $QF 204
+#             # lldb -- Index $PWD $QF 204
 #         done
 #     done
 # done
-mkdir -p "Experiments/Bulk/L7e7-Q8e3"
-./Index $PWD "Bulk/L7e7-Q8e3" 128 128
-# lldb -- Index $PWD "Bulk/L7e7-Q8e3" 128 128
+mkdir -p "Experiments/Bulk/Q8e3"
+./Index $PWD "Bulk/Q8e3" 204
+# lldb -- Index $PWD "Bulk/L7e7-Q8e3" 204
